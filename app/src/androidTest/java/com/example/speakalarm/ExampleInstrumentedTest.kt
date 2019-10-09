96c536e56e7a8ed61695c2ff98655973f9794ce5
hinonoir <tanegashimav48273@gmail.com>
2019-10-09 09:50:06 +0900

    パッケージ名を変更（com.example.speakalarm → com.hino.speakalarm）

--- app/src/androidTest/java/com/example/speakalarm/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/speakalarm/ExampleInstrumentedTest.kt
@@ -1,12 +1,10 @@
-package com.example.speakalarm
+package com.hino.speakalarm
 
 import androidx.test.InstrumentationRegistry
 import androidx.test.runner.AndroidJUnit4
-
+import org.junit.Assert.assertEquals
 import org.junit.Test
 import org.junit.runner.RunWith
-
-import org.junit.Assert.*
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -19,6 +17,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getTargetContext()
-        assertEquals("com.example.speakalarm", appContext.packageName)
+        assertEquals("com.hino.speakalarm", appContext.packageName)
     }
 }
